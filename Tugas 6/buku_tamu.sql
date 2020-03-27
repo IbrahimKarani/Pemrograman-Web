f92e4330890d89fd17226216e68d0b0e310444c1
IbrahimKarani <61193755+IbrahimKarani@users.noreply.github.com>
2020-03-27 18:33:08 +0700

    Add files via upload

---
+++ Tugas 6/buku_tamu.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2020 at 12:32 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku_tamu`
+--
+
+CREATE TABLE `buku_tamu` (
+  `id_bt` int(10) NOT NULL,
+  `nama` varchar(200) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `isi` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku_tamu`
+--
+
+INSERT INTO `buku_tamu` (`id_bt`, `nama`, `email`, `isi`) VALUES
+(1, 'Hukama', 'hukama@gmail.com', 'hukama hukama'),
+(2, 'Farid', 'farid@gmail.com', 'Farid Chilmi'),
+(3, 'Sandi', 'sandi@gmail.com', 'Sandi Ragil');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  ADD PRIMARY KEY (`id_bt`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku_tamu`
+--
+ALTER TABLE `buku_tamu`
+  MODIFY `id_bt` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
